df4ba0c2f71d2fd59af453dac0aa5a3cdb559721
Kim McGrath <klmcgrath2@gmail.com>
2019-02-28 14:08:38 +0000

    when Kim left

--- routes/todo.js
+++ routes/todo.js
@@ -1,35 +1,55 @@
 var express = require("express");
+var connect = require("./connect");
 var router = express.Router();
 
 let db;
 
-const mongodb = require("mongodb");
+// router.get("/:taskTitle", (req, res, next) => {
+//   const { taskTitle } = req.params;
+//   let tasks = db.collection("tasks");
 
-let uri =
-  "mongodb+srv://appservice:apps3rv1c3@cluster0-k3ydo.mongodb.net/todo?retryWrites=true";
-function connect(database) {
-  return mongodb.MongoClient.connect(uri).then(client => {
-    console.log("Connected to Mongo DB");
-    return client.db(database);
-  });
-}
+//   tasks.find({ taskTitle }).toArray((err, todo) => {
+//     if (err) {
+//       return res.status(500).json({ error: err, message: "Try again" });
+//     }
+
+//     todo.forEach(function(doc) {
+//       //console.log(doc);
+//       res.json({
+//         payload: todo[0]
+//       });
+//     });
+
+//     //connect("tasks").then(database => {
+//     // db = database;
+//     //});
+//   });
+// });
 
 router.post("/", (req, res, next) => {
   let tasks = db.collection("tasks");
-  if (typeof req.body.info !== "string") {
+  if (typeof req.body.taskTitle !== "string") {
     return res.status(422).json({ error: "Write a task" });
   }
-  const { info } = req.body;
-  const newInfo = {
-    info
+
+  console.log(req.body);
+  const taskTitle = req.body.taskTitle;
+
+  const newTaskTitle = {
+    taskTitle
   };
-  tasks.insert(data, (err, result) => {
+
+  console.log(newTaskTitle);
+
+  tasks.insert(newTaskTitle, (err, result) => {
     if (err) throw err;
 
-    res.status(201).json({ payload: result });
+    res.status(201).json({ payload: result }); //returning the first item from postman in the ops section
   });
 });
 
-connect("test").then(database => {
+connect("tasks").then(database => {
   db = database;
 });
+
+module.exports = router;
