bbcca4a98132e4a87f52ebcda2b77a2c5ff0506a
mrbenbot <benlee.thatsme@gmail.com>
2019-05-07 17:14:02 +0100

    end of monday

---
+++ data.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    question: "Reorder the alcoholic drinks from strongest to weakest.",
+    cards: [
+      { text: "Barcardi Spiced Rum", order: 3 },
+      { text: "Famous Grouse Scotch Whiskey", order: 1 },
+      { text: "Bailey’s Original Irish", order: 4 },
+      { text: "Smirnoff red Label Vodka", order: 2 }
+    ],
+    instruction: "most alcoholic --> least alcoholic",
+    tidbit:
+      "did you know? famouse grouse is the best-selling Scotch whisky in Scotland"
+  }
+];
