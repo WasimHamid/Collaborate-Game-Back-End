d77910401f3fadd5aaeaed207311ec2e788f72ea
mrbenbot <benlee.thatsme@gmail.com>
2019-05-09 17:05:40 +0100

    host page now goes between scores and question

---
+++ data3.js
@@ -0,0 +1,122 @@
+module.exports = [
+  {
+    question: "jersey numbers, in numerical order of the these footballers",
+    cards: [
+      { text: "Lionel Messi", order: 4 },
+      { text: "Banks", order: 1 },
+      { text: "Ronaldo", order: 2 },
+      { text: "Suarez", order: 3 }
+    ],
+    instruction: "lowest number --> highest number",
+    tidbit: "Because this is really important to know"
+  },
+
+  {
+    question: "Continents largest to smallest",
+    cards: [
+      { text: "Asia", order: 1 },
+      { text: "Africa", order: 2 },
+      { text: "North America", order: 3 },
+      { text: "South America", order: 4 }
+    ],
+    instruction: "largest land mass --> smallest land mass",
+    tidbit: "Driving across them would take a long time"
+  },
+
+  {
+    question: "Highest grossing movies of all time as of 2018",
+    cards: [
+      { text: "Avatar", order: 1 },
+      { text: "Titanic", order: 2 },
+      { text: "Star Wars: The Force Awakens", order: 3 },
+      { text: "Avengers: Infinity War", order: 4 }
+    ],
+    instruction: "highest grossing --> lowest grossing",
+    tidbit: "Combined, they have earned "
+  },
+
+  {
+    question: "Inventions in order of invention (start with earliest)",
+    cards: [
+      { text: "telescope", order: 1 },
+      { text: "spinning jenny", order: 2 },
+      { text: "morse code", order: 3 },
+      { text: "penny farthing", order: 4 }
+    ],
+    instruction: "oldest --> newest",
+    tidbit: "... --- ..."
+  },
+
+  {
+    question: "List capitals from shortest word to longest word",
+    cards: [
+      { text: "Russia", order: 1 },
+      { text: "Brazil", order: 2 },
+      { text: "Iceland", order: 3 },
+      { text: "New Zealand", order: 4 }
+    ],
+    instruction: "fewest letters --> most letters",
+    tidbit: "It's not Lima"
+  },
+
+  {
+    question: "Harry Potter spells from strongest to weakest",
+    cards: [
+      { text: "Expecto Patronum", order: 1 },
+      { text: "Avada kedavra", order: 2 },
+      { text: "Imperius curse", order: 3 },
+      { text: "Cruciatus curse", order: 4 }
+    ],
+    instruction: "strongest --> weekest",
+    tidbit: "Abracadabra"
+  },
+
+  {
+    question: "Bootcamper 2 heights shortest to tallest",
+    cards: [
+      { text: "Karen", order: 1 },
+      { text: "Kim", order: 2 },
+      { text: "Rhys", order: 3 },
+      { text: "Ben", order: 4 }
+    ],
+    instruction: "shortest --> tallest",
+    tidbit: "feet and inches "
+  },
+
+  {
+    question: "Most UK number one hits - most to least",
+    cards: [
+      { text: "Elvis", order: 1 },
+      { text: "Beatles", order: 2 },
+      { text: "Cliff Richard", order: 3 },
+      { text: "ABBA", order: 4 }
+    ],
+    instruction: "most number 1s  --> least number 1s",
+    tidbit: "Hey Jude"
+  },
+
+  {
+    question:
+      "Most expensive paintings ever sold, most expensive to least expensive",
+    cards: [
+      { text: "Salvator Mundi by Leonardi DaVinci", order: 1 },
+      { text: "Interchange by Willem de Kooning", order: 2 },
+      { text: "The Card Players by Paul Cezane", order: 3 },
+      { text: "Nafea Faa Ipoipo by Paul Gauguin", order: 4 }
+    ],
+    instruction: "most expensive --> least expensive",
+    tidbit: "Dogs Playing Poker anyone?"
+  },
+
+  {
+    question: "Most venomous snakes, most venomous to least venomous",
+    cards: [
+      { text: "Fierce", order: 1 },
+      { text: "Eastern Brown Snake", order: 2 },
+      { text: "Blue Krait", order: 3 },
+      { text: "Taipan", order: 4 }
+    ],
+    instruction: "most venomous --> least venomous",
+    tidbit: "ssssssssssssssssss"
+  }
+];
