9e0e7f3d4f5d1316c0d142b823e458693f940a16
mrbenbot <benlee.thatsme@gmail.com>
2019-05-14 14:56:06 +0100

    new questions and scoring system

---
+++ energiser.js
@@ -0,0 +1,116 @@
+module.exports = [
+  {
+    roundinfo: "Wow, they could really sing!",
+    question: "List these celebrity deaths starting with the most recent",
+    cards: [
+      { text: "Freddie Mercury", order: 4 },
+      { text: "Michael Jackson", order: 3 },
+      { text: "Whitney Houston", order: 2 },
+      { text: "George Michael", order: 1 }
+    ],
+    instruction: ["Recently Dead", "Died long ago"],
+    tidbit:
+      "George Michael was a frequent user of the good old british public convienence!"
+  },
+
+  {
+    roundinfo: "Aey, E I Owe yoU a tenner!",
+    question:
+      "Order these words by how many vowels they contain.  Start with the highest number of vowels!",
+    cards: [
+      { text: "oceanfront", order: 2 },
+      { text: "hungry", order: 4 },
+      { text: "positivity", order: 3 },
+      { text: "rationalise", order: 1 }
+    ],
+    instruction: ["Highest No vowels", "Lowest No vowels"],
+    tidbit:
+      "Euouae, at six letters long, is the longest English word consisting only of vowels!"
+  },
+
+  {
+    roundinfo: "Hollywood or bust!",
+    question: "List these movie stars by who has been married the most!",
+    cards: [
+      { text: "Zsa Zsa Gabor", order: 1 },
+      { text: "Elizabeth Taylor", order: 2 },
+      { text: "Joan Collins", order: 3 },
+      { text: "Demi Moore", order: 4 }
+    ],
+    instruction: ["Most married", "Least married"],
+    tidbit: "Elizabeth Taylor was married to Conrad Hilton for only 205 days!"
+  },
+
+  {
+    roundinfo: "I could take him!......I think?",
+    question:
+      "Order these superheroes by their strength.  Start with the strongest!",
+    cards: [
+      { text: "The Thing", order: 3 },
+      { text: "Colossus", order: 4 },
+      { text: "Hulk", order: 1 },
+      { text: "Thor", order: 2 }
+    ],
+    instruction: ["Strongest", "Weakest"],
+    tidbit:
+      "Colossus is made from a substance resembling steel. It is of unknown composition, but appears to be analogous to osmium and carbon steel."
+  },
+
+  {
+    roundinfo: "Motorway Madness!",
+    question: "Order these UK motorways starting with the shortest!",
+    cards: [
+      { text: "M5", order: 1 },
+      { text: "M4", order: 2 },
+      { text: "M1", order: 3 },
+      { text: "M6", order: 4 }
+    ],
+    instruction: ["Shortest", "Longest"],
+    tidbit:
+      "In August 2017, the M5 in Somerset, saw the longest disruption and biggest tailback stretching for 36 miles!"
+  },
+
+  {
+    roundinfo: "One sheep, Two sheep, Three sheep...",
+    question:
+      "Order the answers of these maths questions from lowest to highest.",
+    cards: [
+      { text: "Divide 240 by 20", order: 2 },
+      { text: "Square root of 256", order: 4 },
+      { text: "2 cubed", order: 1 },
+      { text: "4 + 5 x 2", order: 3 }
+    ],
+    instruction: ["Lowest", "Highest"],
+    tidbit:
+      "The youngest person ever to pass A level maths with a Grade A was, Zohaib Ahmed, at the age of 9."
+  },
+
+  {
+    roundinfo: "Fourrrrrrrr!........",
+    question: "Order these golf hole scores beginning with the best. ",
+    cards: [
+      { text: "Condor", order: 1 },
+      { text: "Eagle", order: 3 },
+      { text: "Birdie", order: 4 },
+      { text: "Albatross", order: 2 }
+    ],
+    instruction: ["Best", "Worst"],
+    tidbit:
+      "Apparently, about 20 million balls are lost every year in water hazards on British golf courses!"
+  },
+
+  {
+    roundinfo: "Diesel, Petrol or Hybrid?",
+    question:
+      "Order these car manufacturers from most reliable to least reliable as recorded in AutoExpress 2018. ",
+    cards: [
+      { text: "Mercedes", order: 3 },
+      { text: "Lexus", order: 1 },
+      { text: "Honda", order: 4 },
+      { text: "Jaguar", order: 2 }
+    ],
+    instruction: ["Most Reliable", "Least Reliable"],
+    tidbit:
+      "The Rolls-Royce Phantom VIII is the most expensive Uk series production road car at £373,824!"
+  }
+];
