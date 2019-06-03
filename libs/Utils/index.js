c8da543f1843878a42529b3846d40327d81525aa
mrbenbot <benlee.thatsme@gmail.com>
2019-06-03 16:18:42 +0100

    changed game joining logic and picture round and scoreing

--- libs/Utils/index.js
+++ libs/Utils/index.js
@@ -1,35 +1,36 @@
 function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
+	var currentIndex = array.length,
+		temporaryValue,
+		randomIndex;
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
 }
 
 function getShuffledArray() {
-  return shuffle([0, 1, 2, 3]);
+	return shuffle([0, 1, 2, 3]);
 }
 
 function getNewRoomId() {
-  return Math.floor(Math.random() * 9000 + 1000);
+	return Math.floor(Math.random() * 9000 + 1000);
 }
 
 function getRoomReadyForSending(room) {
-  const { intervalIdCountdown, intervalIdRound, ...dataToSend } = room;
-  return dataToSend;
+	const { intervalIdCountdown, intervalIdRound, ...dataToSend } = room;
+	return dataToSend;
 }
 
 module.exports = {
-  getShuffledArray,
-  getNewRoomId,
-  getRoomReadyForSending
+	getShuffledArray,
+	getNewRoomId,
+	getRoomReadyForSending,
+	shuffle
 };
