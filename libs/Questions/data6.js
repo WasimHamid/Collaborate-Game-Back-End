b9f5b9846931e1c49853a559a46843ce42e507d0
mrbenbot <benlee.thatsme@gmail.com>
2019-06-04 15:41:45 +0100

    captain submit logic

--- libs/Questions/data6.js
+++ libs/Questions/data6.js
@@ -1,77 +1,78 @@
 module.exports = [
-	{
-		question: "What is this persons first name?",
-		roundinfo: "Jigsaw Pictionary",
-		cards: [
-			{ url: "https://i.imgur.com/2vVvmOz.jpg" },
-			{ url: "https://i.imgur.com/UlM1UEh.jpg" },
-			{ url: "https://i.imgur.com/yG5Z7KJ.jpg" },
-			{ url: "https://i.imgur.com/VC91LQp.jpg" }
-		],
-		answer: "ALEX",
-		answerURL:
-			"https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
-		questionType: "picture"
-	},
-	{
-		question: "Put these botcampers in age order",
-		roundinfo: "Old News",
-		cards: [
-			{ text: "Aiden", order: 4 },
-			{ text: "Kathryn", order: 1 },
-			{ text: "Brendan", order: 2 },
-			{ text: "Stewart", order: 3 }
-		],
-		instruction: ["Oldest", "Youngest"],
-		questionType: "order"
-	},
+  {
+    question: "What is this persons first name?",
+    roundinfo: "Jigsaw Pictionary",
+    cards: [
+      { url: "https://i.imgur.com/2vVvmOz.jpg" },
+      { url: "https://i.imgur.com/UlM1UEh.jpg" },
+      { url: "https://i.imgur.com/yG5Z7KJ.jpg" },
+      { url: "https://i.imgur.com/VC91LQp.jpg" }
+    ],
+    answer: "ALEX",
+    answerURL:
+      "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+    questionType: "picture"
+  },
+  {
+    question: "Put these botcampers in age order",
+    roundinfo: "Old News",
+    cards: [
+      { text: "Aiden", order: 4 },
+      { text: "Kathryn", order: 1 },
+      { text: "Brendan", order: 2 },
+      { text: "Stewart", order: 3 }
+    ],
+    instruction: ["Oldest", "Youngest"],
+    questionType: "order"
+  },
 
-	{
-		question: "Whos Birthday is Soonest",
-		roundinfo: "Born Identity",
-		cards: [
-			{ text: "Ben", order: 2 },
-			{ text: "Brendan", order: 4 },
-			{ text: "Stuart", order: 3 },
-			{ text: "Wasim", order: 1 }
-		],
-		instruction: ["soonest", "least soon"],
-		questionType: "order"
-	},
+  {
+    question: "Whos Birthday is Soonest",
+    roundinfo: "Born Identity",
+    cards: [
+      { text: "Ben", order: 2 },
+      { text: "Brendan", order: 4 },
+      { text: "Stuart", order: 3 },
+      { text: "Wasim", order: 1 }
+    ],
+    instruction: ["soonest", "least soon"],
+    questionType: "order"
+  },
 
-	{
-		question: "css prowess: likes it the most to hates it the most",
-		roundinfo: "Cascading Style Shits",
-		cards: [
-			{ text: "matt", order: 1 },
-			{ text: "manu", order: 2 },
-			{ text: "alex", order: 3 },
-			{ text: "bukola", order: 4 }
-		],
-		instruction: ["loves most", "hates most"],
-		questionType: "order"
-	},
-	{
-		question: "where are they from? born furthest away to nearest:",
-		roundinfo: "Born Ultimatum",
-		cards: [
-			{ text: "manu", order: 1 },
-			{ text: "kim", order: 2 },
-			{ text: "robyn", order: 3 },
-			{ text: "stu", order: 4 }
-		],
-		instruction: ["furthest", "nearest"],
-		questionType: "order"
-	},
-	{
-		question: "What is this persons first name?",
-		roundinfo: "Jigtionary... Y Not?",
-		cards: [
-			{ url: "https://i.imgur.com/jsorCzo.jpg" },
-			{ url: "https://i.imgur.com/PowPXCp.jpg" },
-			{ url: "https://i.imgur.com/gS0LOYY.jpg" },
-			{ url: "https://i.imgur.com/naLAgDX.jpg" }
-		],
-		answer: "KATHRYN"
-	}
+  {
+    question: "css prowess: likes it the most to hates it the most",
+    roundinfo: "Cascading Style Shits",
+    cards: [
+      { text: "matt", order: 1 },
+      { text: "manu", order: 2 },
+      { text: "alex", order: 3 },
+      { text: "bukola", order: 4 }
+    ],
+    instruction: ["loves most", "hates most"],
+    questionType: "order"
+  },
+  {
+    question: "where are they from? born furthest away to nearest:",
+    roundinfo: "Born Ultimatum",
+    cards: [
+      { text: "manu", order: 1 },
+      { text: "kim", order: 2 },
+      { text: "robyn", order: 3 },
+      { text: "stu", order: 4 }
+    ],
+    instruction: ["furthest", "nearest"],
+    questionType: "order"
+  },
+  {
+    question: "What is this persons first name?",
+    roundinfo: "Jigtionary... Y Not?",
+    cards: [
+      { url: "https://i.imgur.com/jsorCzo.jpg" },
+      { url: "https://i.imgur.com/PowPXCp.jpg" },
+      { url: "https://i.imgur.com/gS0LOYY.jpg" },
+      { url: "https://i.imgur.com/naLAgDX.jpg" }
+    ],
+    answer: "KATHRYN",
+    questionType: "picture"
+  }
 ];
